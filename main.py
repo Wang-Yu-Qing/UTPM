15fb4a5a33316cdb98dd034581240de47b5cf6b1
413022472@qq.com <413022472@qq.com>
2021-10-06 22:39:14 +0800

    generate samples

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from utils import *
 
 
 movie_tag = extract_movie_tag_relation("data/ml-20m/genome-scores.csv")
-user_movie = extract_user_movie_relation("data/ml-20m/ratings.csv")
+movie_cate, cate_encoder, cate_decoder = extract_movie_cate_relation("data/ml-20m/movies.csv")
+user_behaviors = extract_user_behaviors("data/ml-20m/ratings.csv")
 
-train_users, test_users = split_users(user_movie.keys())
+train_users, test_users = split_users(user_behaviors.keys())
+
+train_samples, test_samples = generate_samples(train_users, test_users, user_behaviors, movie_tag, movie_cate)
--- utils.py
+++ utils.py
@@ -3,19 +3,9 @@ from sklearn.model_selection import train_test_split
 import random
 
 
-class User:
-    def __init__(self, id):
-        self.id = id
-        self.history_tags = []   
-        self.history_cates = []
-    
-    def generate_features():
-        pass
-
-
 def extract_tags(tag_scores, movie_tag_rel, last_movie_id, top):
     tags = sorted(tag_scores, key=lambda x: x[1])[-top:]
-    movie_tag_rel[last_movie_id] = [x[1] for x in tags]
+    movie_tag_rel[last_movie_id] = [x[0] for x in tags]
     tag_scores.clear()
 
 
@@ -25,6 +15,7 @@ def extract_movie_tag_relation(filepath, top=10):
         f.readline()
         last_movie_id, tag_scores = None, []
         for line in f.readlines():
+            line = line.strip()
             splitted = line.split(",")
             movie_id, tag_id, score = splitted[0], splitted[1], float(splitted[2])
             if last_movie_id is not None and movie_id != last_movie_id:
@@ -37,21 +28,57 @@ def extract_movie_tag_relation(filepath, top=10):
     return movie_tag_rel
 
 
-def extract_user_movie_relation(filepath):
-    user_movie_rel = {}
+def extract_movie_cate_relation(filepath):
+    cate_encoder, cate_decoder, cate_id = {}, [], 0
+    movie_cate_rel = {}
+    with open(filepath, "r", encoding="utf-8") as f:
+        f.readline()
+        for line in f.readlines():
+            cates_encoded = []
+            line = line.strip()
+            splitted = line.split(",")
+            cates, movie_id = splitted[-1].split("|"), splitted[0]
+            for cate in cates:
+                if cate in cate_encoder:
+                    cates_encoded.append(cate_encoder[cate])
+                else:
+                    cates_encoded.append(cate_id)
+                    cate_encoder[cate] = cate_id
+                    cate_decoder.append(cate)
+                    cate_id += 1
+            movie_cate_rel[movie_id] = cates_encoded
+    
+    return movie_cate_rel, cate_encoder, cate_decoder
+            
+
+def extract_user_behaviors(filepath):
+    user_behaviors = {}
+    pos, neg = 0, 0
     with open(filepath, "r") as f:
         f.readline()
         for line in f.readlines():
+            line = line.strip()
             splitted = line.split(",")
-            user_id, movie_id, rating = splitted[0], splitted[1], float(splitted[2])
-            if user_id not in user_movie_rel:
-                user_movie_rel[user_id] = []
+            user_id, movie_id, rating, timestamp = splitted[0], splitted[1], float(splitted[2]), int(splitted[3])
+            if user_id not in user_behaviors:
+                user_behaviors[user_id] = []
             if rating <= 1.5:
-                user_movie_rel[user_id].append((movie_id, 0))
+                user_behaviors[user_id].append((movie_id, 0, timestamp))
+                neg += 1
             elif rating >= 3.5:
-                user_movie_rel[user_id].append((movie_id, 1))
-    
-    return user_movie_rel
+                user_behaviors[user_id].append((movie_id, 1, timestamp))
+                pos += 1
+    print("pos: {}, neg: {}".format(pos, neg)) 
+
+    for user_id, behavior in user_behaviors.items():
+        # sort behavior by time, use top 80% to build history feature 
+        # and last 20% as label
+        behavior_sorted = sorted(behavior, key=lambda x: x[2])
+        pivot = int(len(behavior) * 0.8)
+        X, Y = behavior[:pivot], behavior[pivot:]
+        user_behaviors[user_id] = {"X": X, "Y": Y}
+
+    return user_behaviors
 
 
 def split_users(users):
@@ -65,3 +92,53 @@ def split_users(users):
 
     return train, test
 
+
+def generate_user_samples(user_id, histories, futures, movie_tag_map, movie_cate_map, samples):
+    # build features
+    # list features no deduplicating
+    pos_tags, neg_tags, pos_cates, neg_cates = [], [], [], []
+    for movie_id, action, timestamp in histories:
+        try:
+            movie_tags, movie_cates = movie_tag_map[movie_id], movie_cate_map[movie_id]
+        except KeyError:
+            continue
+        if action == 1:
+            pos_tags += movie_tags
+            pos_cates += movie_cates
+        else:
+            neg_tags += movie_tags
+            neg_cates += movie_cates
+    # each user has one X
+    X = (user_id, pos_tags, neg_tags, pos_cates, neg_cates)
+
+    # build labels, each user has multi Ys
+    for movie_id, action, timestamp in futures:
+        try:
+            movie_tags = movie_tag_map[movie_id]
+        except KeyError:
+            continue
+        samples.append([X, (movie_tags, action)])
+
+
+def generate_samples(train_users, test_users, user_behaviors, movie_tag_map, movie_cate_map):
+    train_samples, test_samples = [], []
+    for user_id in train_users:
+        histories, futures = user_behaviors[user_id]["X"], user_behaviors[user_id]['Y']
+        generate_user_samples(user_id, 
+                              histories, 
+                              futures, 
+                              movie_tag_map, 
+                              movie_cate_map, 
+                              train_samples)
+
+    for user_id in test_users:
+        histories, futures = user_behaviors[user_id]["X"], user_behaviors[user_id]['Y']
+        generate_user_samples(user_id, 
+                              histories, 
+                              futures, 
+                              movie_tag_map, 
+                              movie_cate_map, 
+                              test_samples)
+    
+    return train_samples, test_samples
+ 
